149a8eff25931e4d6084c56eb4b53a9cfb0cf022
Yurii Ponomarenko <ponomy@gmail.com>
2021-08-20 19:10:44 +0200

    Create ReportsAnalyst.ps1
    Creates summary from reports which created by the cve-mitigation script.

---
+++ ReportsAnalyst.ps1
@@ -0,0 +1,28 @@
+$ReportsPath = '\\amznfsxcsey61pp.home24.lan\share\Logs\CVE-2021-36934\'
+
+$VSSErrorFixed = 0
+[psobject[]]$Results = @()
+
+foreach ($f in (Get-childItem -LiteralPath $ReportsPath -File)) {
+    Write-Host '.' -NoNewline
+    $PermOk = 'FAILED'
+    $VSC = 'Unknown'
+    [string[]]$text = Get-Content -LiteralPath $f.FullName -Encoding unicode
+    # $text
+    if (($text | ? {$_ -like "*All Ok*"})) { $PermOk = 'Ok' }
+    if (($text | ? {$_ -like "*VSS Ok*"})) {
+        $VSC = 'Ok' 
+        if (($text | ? {$_ -like "*VSS FAILED*"})) { $VSSErrorFixed++ }
+    } else {
+        if (($text | ? {$_ -like "*VSS FAILED*"})) { $VSC = 'FAILED' }
+    }
+    $Results += New-Object –TypeName PSObject -Property ([ordered]@{
+        'ComputerName' = $f.Name.Substring(0, $f.Name.IndexOf('_'))
+        'Permissions'  = $PermOk
+        'VSCRemoved'   = $VSC
+    })
+}
+'='*100
+$Results | sort permissions, name, VSCRemoved
+"Total VSC fixed: $VSSErrorFixed"
+$Results | Export-Csv -LiteralPath c:\tmp\ReportsAnalyse.csv -Encoding unicode -Force -Delimiter ','
